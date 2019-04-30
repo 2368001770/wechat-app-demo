63e26b5200811a9ed9429f7e24811b23c00a60df
czj <2368001770@qq.com>
2019-04-30 15:13:03 +0800

    sql file

---
+++ sql/tb_area.sql
@@ -0,0 +1,40 @@
+/*
+SQLyog Ultimate v11.33 (64 bit)
+MySQL - 5.6.34 : Database - wechat_app_demo
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`wechat_app_demo` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `wechat_app_demo`;
+
+/*Table structure for table `tb_area` */
+
+DROP TABLE IF EXISTS `tb_area`;
+
+CREATE TABLE `tb_area` (
+  `area_id` int(2) NOT NULL AUTO_INCREMENT,
+  `area_name` varchar(200) NOT NULL,
+  `priority` int(2) NOT NULL DEFAULT '0',
+  `create_time` datetime DEFAULT NULL,
+  `last_edit_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`area_id`),
+  UNIQUE KEY `UK_AREA` (`area_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+
+/*Data for the table `tb_area` */
+
+insert  into `tb_area`(`area_id`,`area_name`,`priority`,`create_time`,`last_edit_time`) values (1,'西苑',2,'2018-11-01 21:49:27','2018-11-01 21:49:31'),(2,'东苑',1,'2018-11-01 21:49:45','2018-12-01 21:49:48'),(3,'南苑',7,'2019-02-01 21:50:02','2019-04-30 00:50:17'),(4,'北苑',3,'2019-03-01 21:50:18','2019-04-01 21:50:22');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
